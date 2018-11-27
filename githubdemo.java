937674cce2e1e0e4b21a228d7d7afdfa10e98525
SRajan13 <45398968+SRajan13@users.noreply.github.com>
2018-11-27 12:12:22 -0500

    added print statement
    added comment and print statement

--- githubdemo.java
+++ githubdemo.java
@@ -2,8 +2,9 @@ public class githubdemo{
 	public static void main(String[]args){
 
 	System.out.println("Hello World");
-	System.out.print("hello");
-	System.out.print("hello");
-
+	System.out.println("hello");
+	System.out.println("hello");
+	//hello
+	System.out.println("What's up");
 	}
 }
